2add09bc7964d1f28c5c2137ebb7ac6f68876e27
Edward Effendy <edwardeffendy07@gmail.com>
2023-02-10 10:18:05 +0900

    New changes to settings

--- FINALPROJECT/settings.py
+++ FINALPROJECT/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 import dj_database_url
-import django-heroku
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
